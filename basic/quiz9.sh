e04515e0595dcefae5ad95e50fe2d146626c3a12
root <@gmail.com>
2024-04-04 11:40:20 +0900

    quiz9

--- basic/quiz9.sh
+++ basic/quiz9.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 touch multiLine.txt
-cat > multiLine.txt
-\n
-\n
-\n
-\n
-\n
-\n
-\n
-head -n 3 multiLine.txt
+echo -e "Line1\nLine2\nLine3\nLine4\nLine5\nLine6\nLine7\nLine8\nLine9\nLine10" >> multiLine.txt
+head -3 multiLine.txt
